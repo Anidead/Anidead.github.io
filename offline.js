3b258e5e1dba98a93bc0543be44d92980d3496cf
Anidead <30629188+Anidead@users.noreply.github.com>
2017-08-01 17:48:56 +0300

    1

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1501598668,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tilemap.png",
+		"images/ground.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/player-sheet2.png",
+		"images/bg1.png",
+		"images/coinbox-sheet0.png",
+		"images/coinbox-sheet1.png",
+		"images/coin-sheet0.png",
+		"images/coinitem-sheet0.png",
+		"images/buttonleft-sheet0.png",
+		"images/btnright-sheet0.png",
+		"images/btnjump-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
